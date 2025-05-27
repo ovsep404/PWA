be2c70c531905d61d834f759d5fc7dfb45c4e1b3
ovsmar <ovsep.simonian@gmail.com>
2025-05-27 16:01:13 +0200

    pwa

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "pwa-v1";
+const urlsToCache = ["./", "./index.html", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Add push notification handler
+self.addEventListener("push", (event) => {
+  const options = {
+    body: event.data.text(),
+    icon: "icons/icon.png",
+    badge: "icons/icon.png",
+  };
+
+  event.waitUntil(
+    self.registration.showNotification("PWA Notification", options)
+  );
+});
